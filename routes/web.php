200b5592ea3f92c8468f664f3fa51e09ea7b5dd1
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-23 07:29:19 +0530

    Connect public pages to CMS data

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,16 @@ use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\ContentController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\InstitutionController;
+use App\Http\Controllers\PublicSiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', [PublicSiteController::class, 'home'])->name('home');
 Route::view('/about', 'pages.about')->name('about');
-Route::view('/institutions', 'pages.institutions')->name('institutions');
+Route::get('/institutions', [PublicSiteController::class, 'institutions'])->name('institutions');
 Route::view('/programs', 'pages.programs')->name('programs');
-Route::view('/news-events', 'pages.news-events')->name('news-events');
-Route::view('/gallery', 'pages.gallery')->name('gallery');
-Route::view('/downloads', 'pages.downloads')->name('downloads');
+Route::get('/news-events', [PublicSiteController::class, 'news'])->name('news-events');
+Route::get('/gallery', [PublicSiteController::class, 'gallery'])->name('gallery');
+Route::get('/downloads', [PublicSiteController::class, 'downloads'])->name('downloads');
 Route::view('/career', 'pages.career')->name('career');
 Route::view('/contact', 'pages.contact')->name('contact');
 
