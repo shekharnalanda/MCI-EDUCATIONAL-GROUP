1a6ea3350f8a8ef06368e3b75912b8060c1cc511
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-08-29 11:52:49 +0530

    Wire admissions customers communications follow-ups and auto reply test routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\AutoReplyController;
+use App\Http\Controllers\Admin\CentralAdmissionController;
 use App\Http\Controllers\Admin\CentralEnquiryController;
 use App\Http\Controllers\Admin\ContentController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\InstitutionController;
+use App\Http\Controllers\Admin\OperationsController;
 use App\Http\Controllers\EnquiryController;
 use App\Http\Controllers\PublicSiteController;
 use Illuminate\Support\Facades\Route;
@@ -37,21 +39,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::post('/institutions/{institution}/api-token', [InstitutionController::class, 'generateToken'])->name('institutions.api-token');
         Route::delete('/institutions/{institution}', [InstitutionController::class, 'destroy'])->name('institutions.destroy');
 
-        Route::get('/news', [ContentController::class, 'news'])->name('news.index');
-        Route::post('/news', [ContentController::class, 'storeNews'])->name('news.store');
-        Route::put('/news/{newsPost}', [ContentController::class, 'updateNews'])->name('news.update');
-        Route::delete('/news/{newsPost}', [ContentController::class, 'deleteNews'])->name('news.destroy');
-
-        Route::get('/gallery', [ContentController::class, 'gallery'])->name('gallery.index');
-        Route::post('/gallery', [ContentController::class, 'storeGallery'])->name('gallery.store');
-        Route::put('/gallery/{galleryItem}', [ContentController::class, 'updateGallery'])->name('gallery.update');
-        Route::delete('/gallery/{galleryItem}', [ContentController::class, 'deleteGallery'])->name('gallery.destroy');
-
-        Route::get('/downloads', [ContentController::class, 'downloads'])->name('downloads.index');
-        Route::post('/downloads', [ContentController::class, 'storeDownload'])->name('downloads.store');
-        Route::put('/downloads/{download}', [ContentController::class, 'updateDownload'])->name('downloads.update');
-        Route::delete('/downloads/{download}', [ContentController::class, 'deleteDownload'])->name('downloads.destroy');
-
         Route::get('/enquiries', [CentralEnquiryController::class, 'index'])->name('enquiries.index');
         Route::get('/enquiries/{enquiry}', [CentralEnquiryController::class, 'show'])->name('enquiries.show');
         Route::patch('/enquiries/{enquiry}', [CentralEnquiryController::class, 'update'])->name('enquiries.update');
@@ -59,12 +46,36 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::post('/enquiries/{enquiry}/follow-up', [CentralEnquiryController::class, 'scheduleFollowUp'])->name('enquiries.follow-up');
         Route::delete('/enquiries/{enquiry}', [CentralEnquiryController::class, 'destroy'])->name('enquiries.destroy');
 
+        Route::get('/admissions', [CentralAdmissionController::class, 'index'])->name('admissions.index');
+        Route::get('/admissions/{admission}', [CentralAdmissionController::class, 'show'])->name('admissions.show');
+        Route::patch('/admissions/{admission}', [CentralAdmissionController::class, 'update'])->name('admissions.update');
+        Route::delete('/admissions/{admission}', [CentralAdmissionController::class, 'destroy'])->name('admissions.destroy');
+
+        Route::get('/customers', [OperationsController::class, 'customers'])->name('customers.index');
+        Route::get('/customers/{customer}', [OperationsController::class, 'customer'])->name('customers.show');
+        Route::get('/communications', [OperationsController::class, 'communications'])->name('communications.index');
+        Route::get('/follow-ups', [OperationsController::class, 'followUps'])->name('follow-ups.index');
+        Route::patch('/follow-ups/{followUp}/complete', [OperationsController::class, 'completeFollowUp'])->name('follow-ups.complete');
+
         Route::get('/auto-replies', [AutoReplyController::class, 'index'])->name('auto-replies.index');
+        Route::post('/auto-replies/test', [AutoReplyController::class, 'test'])->name('auto-replies.test');
         Route::post('/auto-replies/templates', [AutoReplyController::class, 'storeTemplate'])->name('auto-replies.templates.store');
         Route::post('/auto-replies/rules', [AutoReplyController::class, 'storeRule'])->name('auto-replies.rules.store');
         Route::patch('/auto-replies/rules/{rule}/toggle', [AutoReplyController::class, 'toggleRule'])->name('auto-replies.rules.toggle');
         Route::patch('/auto-replies/business/{institution}/toggle', [AutoReplyController::class, 'toggleBusiness'])->name('auto-replies.business.toggle');
 
+        Route::get('/news', [ContentController::class, 'news'])->name('news.index');
+        Route::post('/news', [ContentController::class, 'storeNews'])->name('news.store');
+        Route::put('/news/{newsPost}', [ContentController::class, 'updateNews'])->name('news.update');
+        Route::delete('/news/{newsPost}', [ContentController::class, 'deleteNews'])->name('news.destroy');
+        Route::get('/gallery', [ContentController::class, 'gallery'])->name('gallery.index');
+        Route::post('/gallery', [ContentController::class, 'storeGallery'])->name('gallery.store');
+        Route::put('/gallery/{galleryItem}', [ContentController::class, 'updateGallery'])->name('gallery.update');
+        Route::delete('/gallery/{galleryItem}', [ContentController::class, 'deleteGallery'])->name('gallery.destroy');
+        Route::get('/downloads', [ContentController::class, 'downloads'])->name('downloads.index');
+        Route::post('/downloads', [ContentController::class, 'storeDownload'])->name('downloads.store');
+        Route::put('/downloads/{download}', [ContentController::class, 'updateDownload'])->name('downloads.update');
+        Route::delete('/downloads/{download}', [ContentController::class, 'deleteDownload'])->name('downloads.destroy');
         Route::get('/settings', [ContentController::class, 'settings'])->name('settings.index');
         Route::put('/settings', [ContentController::class, 'saveSettings'])->name('settings.update');
     });
